956e5e6adc4abea7c2e824006517b95f2a7784c9
ThienStai <thienplaygame98@gmail.com>
2020-11-20 12:01:44 +0700

    Fix buggy comments

--- mess-attack-plus.py
+++ mess-attack-plus.py
@@ -2,40 +2,41 @@ from pyautogui import typewrite, FAILSAFE, press, MINIMUM_DURATION, FailSafeExce
 from time import sleep
 from os import system
 from pyinputplus import inputInt, inputFloat
+
 print("\nWelcome to MessAttack v1.0 by thien")
 print("\nChoose a attack mode to begin:\n")
 print("unsafe : type as fast as possible, may cause lag")
 print("safe   : type with delay and fail-safe")
 option = input("\nYour option: ").lower()
 MINIMUM_DURATION = 0.0001
+
+
 def countdown():
     for i in range(3):
         print("Attack start in %s ") % i
         sleep(1)
+
+
 def main():
     global FAILSAFE, FailSafeException, MINIMUM_DURATION
     while True:
         try:
-            ###----------------------UNSAFE GO HERE------------------------###
-            ###----------------------UNSAFE GO HERE------------------------###
-            ###----------------------UNSAFE GO HERE------------------------###
-    
+            # ----------------------UNSAFE GO HERE------------------------
+
             if option.lower() == "unsafe":
                 print("\nIf lag then move your mouse to upper left screen corner to exit!")
                 FAILSAFE = True
                 string_to_spam = input("String to spam = ")
                 times_to_spam = inputInt("Time to spam = ", min=1, max=2147483647)
-                # countdown()
+                countdown()
                 for i in range(0, int(times_to_spam)):
                     typewrite(string_to_spam)
                     press('enter')
-                    print("Spammed %s times, %s spams left") % (i, times_to_spam-i)
+                    print("Spammed %s times, %s spams left") % (i, times_to_spam - i)
                 print("Done!\nThanks for using this app!")
-    
-            ###----------------------SAFE GO HERE------------------------###
-            ###----------------------SAFE GO HERE------------------------###
-            ###----------------------SAFE GO HERE------------------------###
-    
+
+            # ----------------------SAFE GO HERE------------------------
+
             elif option == "safe":
                 print("\nIf lag then move your mouse to upper left screen corner to exit!")
                 FAILSAFE = True
@@ -48,11 +49,15 @@ def main():
                     typewrite(string_to_spam, interval=letter_delay)
                     sleep(enter_delay)
                     press('enter')
-                    print("Spammed %s times, %s spams left" % (i, times_to_spam-i))
+                    print("Spammed %s times, %s spams left" % (i, times_to_spam - i))
                 print("Done!\nThanks for using this app!")
+            else:
+                print("Invalid mode!!")
+                pass
         except FailSafeException:
             print("You canceled the operation!")
             pass
 
+
 if __name__ == '__main__':
-   main()
+    main()
