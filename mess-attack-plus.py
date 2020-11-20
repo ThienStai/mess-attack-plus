93eac7cd67b2bf2fd3fa8b481f2b9c754b46ca53
ThienStai <thienplaygame98@gmail.com>
2020-11-20 18:04:48 +0700

    final touches

--- mess-attack-plus.py
+++ mess-attack-plus.py
@@ -1,29 +1,29 @@
-from pyautogui import typewrite, FAILSAFE, press, MINIMUM_DURATION, FailSafeException
+from pyautogui import alert, typewrite, FAILSAFE, press, MINIMUM_DURATION, FailSafeException
 from time import sleep
 from os import system
 from pyinputplus import inputInt, inputFloat
 
-print("\nWelcome to mess-attack-plus by thien")
-print("\nChoose a attack mode to begin:\n")
-print("unsafe : type as fast as possible, may cause lag")
-print("safe   : type with delay and fail-safe")
-option = input("\nYour option: ").lower()
 MINIMUM_DURATION = 0.0001
 
 
 def countdown():
     for i in range(3):
-        print("Attack start in %s ") % i
+        print("Attack start in %s " % (3-i)) 
         sleep(1)
 
 
 def main():
     global FAILSAFE, FailSafeException, MINIMUM_DURATION
-    while True:
+    Running = True
+    while Running:
         system("cls")
         try:
             # ----------------------UNSAFE GO HERE------------------------
-
+            print("\nWelcome to mess-attack-plus by thien")
+            print("\nChoose a attack mode to begin:\n")
+            print("unsafe : type as fast as possible, may cause lag")
+            print("safe   : type with delay and fail-safe")
+            option = input("\nYour option: ").lower()
             if option.lower() == "unsafe":
                 print("\nIf lag then move your mouse to upper left screen corner to exit!")
                 FAILSAFE = True
@@ -33,11 +33,13 @@ def main():
                 for i in range(0, int(times_to_spam)):
                     typewrite(string_to_spam)
                     press('enter')
-                    print("Spammed %s times, %s spams left") % (i, times_to_spam - i)
+                    print("Spammed %s times, %s spams left" % (i, times_to_spam - i))
                 print("Done!\nThanks for using this app!")
+                sleep(1)
 
             # ----------------------SAFE GO HERE------------------------
-
+            elif option.lower() == "exit":
+                exit(0)
             elif option == "safe":
                 print("\nIf lag then move your mouse to upper left screen corner to exit!")
                 FAILSAFE = True
@@ -52,13 +54,19 @@ def main():
                     press('enter')
                     print("Spammed %s times, %s spams left" % (i, times_to_spam - i))
                 print("Done!\nThanks for using this app!")
+                sleep(1)
             else:
                 print("Invalid mode!!")
-                pass
+                sleep(1)
+                continue
         except FailSafeException:
-            print("You canceled the operation!")
-            pass
+                alert(text="You canceled the attack!", title="Stopped")
+                Running = False
 
 
-if __name__ == '__main__':
-    main()
+try:
+    if __name__ == '__main__':
+        main()
+except KeyboardInterrupt:
+    print("Exiting...")
+    sleep(1)
